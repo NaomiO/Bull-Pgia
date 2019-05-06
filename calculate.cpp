38585deb4edc9b827a0f83ac18e2f3f938132ded
orelrahum <orel.rahum@gmail.com>
2019-05-06 23:18:23 +0300

    f

--- calculate.cpp
+++ calculate.cpp
@@ -3,28 +3,32 @@
 #include <iostream>
 #include <math.h>
 #include "calculate.hpp"
-// used with by Bar Genish and Lioz Elmalem algoritem Idia
+
 namespace bullpgia{
 
   string calculateBullAndPgia(string choice, string guess){
 	if( choice == "" || guess == "" ) throw std::invalid_argument( "The string is empty" );
+		
 	int bull = 0;
 	int pgiot = 0;
-		for (int i=0)
+		
 	for( int i = 0; i < choice.length(); i++ ){  //loop for the chooser
 	  int chooser = choice[i];
 			
-	   	for( int j = 0; j < guess.length(); j++ ){  //loop for the guesser
-				int guesser = guess[j];		
-				if( chooser == guesser && i==j){
-     			bull++; //if the guesser guessed the right number at the right place 
-			 	}
-			}
+	   for( int j = 0; j < guess.length(); j++ ){  //loop for the guesser
+		int guesser = guess[j];
+					
+		if( chooser == guesser){
+		   if( i == j ){     //if the guesser guessed the right number at the right place
+		      bull++; }
+		   else{   pgiot++;  } //if the answer contains the number guessed but not at the right place
+			               }
+						    }
 						  }
 		
 	string bull_str = std::to_string(bull);
         string pgiot_str = std::to_string(pgiot);
         string result = bull_str + "," + pgiot_str;
         return result;
-	 }
+	}
 }
