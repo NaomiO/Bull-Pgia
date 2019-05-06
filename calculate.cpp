76a3861304b79282da4db5db05d2b0bbfba35247
orelrahum <orel.rahum@gmail.com>
2019-05-06 23:21:22 +0300

    d

--- calculate.cpp
+++ calculate.cpp
@@ -11,20 +11,27 @@ namespace bullpgia{
 		
 	int bull = 0;
 	int pgiot = 0;
-		
-	for( int i = 0; i < choice.length(); i++ ){  //loop for the chooser
-	  int chooser = choice[i];
+  int arrguess [10]= {0};
+	int arrcohice [10]= {0};
+
+    for(int i=0;i<choice.length();i++){
+    if(choice[i]==guess[i])
+      bull++;
+  }	
+
+	// for( int i = 0; i < choice.length(); i++ ){  //loop for the chooser
+	//   int chooser = choice[i];
 			
-	   for( int j = 0; j < guess.length(); j++ ){  //loop for the guesser
-		int guesser = guess[j];
+	//    for( int j = 0; j < guess.length(); j++ ){  //loop for the guesser
+	// 	int guesser = guess[j];
 					
-		if( chooser == guesser){
-		   if( i == j ){     //if the guesser guessed the right number at the right place
-		      bull++; }
-		   else{   pgiot++;  } //if the answer contains the number guessed but not at the right place
-			               }
-						    }
-						  }
+	// 	if( chooser == guesser){
+	// 	   if( i == j ){     //if the guesser guessed the right number at the right place
+	// 	      bull++; }
+	// 	   else{   pgiot++;  } //if the answer contains the number guessed but not at the right place
+	// 		               }
+	// 					    }
+	// 					  }
 		
 	string bull_str = std::to_string(bull);
         string pgiot_str = std::to_string(pgiot);
