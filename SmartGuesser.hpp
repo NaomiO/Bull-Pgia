197e5ec720f204c4e7902419e2f08b6458b4a127
orelrahum <orel.rahum@gmail.com>
2019-05-07 22:06:52 +0300

    d

--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -1,18 +1,35 @@
-#pragma once
+#include<iostream>
 #include "Guesser.hpp"
-#include<string>
-using std::string;
-namespace bullpgia {
-class SmartGuesser: public bullpgia::Guesser 
-{
-		string myGuess;
-	public:
-		SmartGuesser() 
-		{		
-			this->myGuess="";	
-		 }
+#include <vector>
 
-		    string guess() override;
-		   
+using namespace std;
+
+namespace bullpgia{
+  class SmartGuesser: public bullpgia::Guesser{
+
+    private :
+    string myGuess;
+    // to learn the solutions
+    uint bull=0;
+    uint pgia=0;
+    uint length=0;
+    int turns=0;
+    bool founddigit=false;
+    bool once=true;
+    string solution="";
+    int guesserhelper=0;
+    public :
+    SmartGuesser(){
+    }
+    SmartGuesser(const string& temp){
+      myGuess = temp;
+      bull = 0;
+      pgia = 0;
+      length=0;
+    }
+    ~SmartGuesser(){}
+    void learn(string solutions) override;
+    string guess() override;
+    void startNewGame(uint length) override;
+  };
 };
- }
